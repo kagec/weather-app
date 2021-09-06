4c11a540b9c0bff56e481744282a86fa4632d3cb
kagec <66302618+kagec@users.noreply.github.com>
2021-09-06 17:27:19 +0900

    `store`ファイル単体でなくフォルダで階層構造にするように変更

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { reducer } from '../reducer';
+
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+export const store = createStore(reducer, enhancer);
