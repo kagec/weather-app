02ef2b3e60dc008aabc14ae571590e3636520208
kagec <66302618+kagec@users.noreply.github.com>
2021-09-21 19:06:52 +0900

    `combineReducer`に`weatherReducer`と`locationReducer`を追加

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
+import { weatherReducer } from "../reducer/weatherReducer";
+import { locationReducer } from "./locationReducer";
+
+export type Reducer = ReturnType<typeof reducer>;
 
 export const reducer = combineReducers({
   init: () => ({}),
+  weather: weatherReducer,
+  location: locationReducer,
 });
