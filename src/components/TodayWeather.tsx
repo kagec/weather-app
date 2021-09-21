841ba4b9377a2e00a71ba180bc151c975366666c
kagec <66302618+kagec@users.noreply.github.com>
2021-09-21 18:24:42 +0900

    `store`からデータを取得

--- src/components/TodayWeather.tsx
+++ src/components/TodayWeather.tsx
@@ -1,6 +1,11 @@
+import { useSelector } from "react-redux";
 import type { VFC } from "react";
+import type { Reducer } from "../reducer";
 
 const TodayWeather: VFC = () => {
+  const todayWeatherData = useSelector((state: Reducer) => state.weather[0]);
+  const location = useSelector((state: Reducer) => state.location);
+
   return <div></div>;
 };
 
