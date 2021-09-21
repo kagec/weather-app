0334f89fe4661db00e47f52a06962f2a8f38b24d
kagec <66302618+kagec@users.noreply.github.com>
2021-09-21 19:05:15 +0900

    `locationReducer`を作成

---
+++ src/reducer/locationReducer.ts
@@ -0,0 +1,23 @@
+import { LOCATION_ACTION_TYPE } from "../action/location";
+import type { Location } from "../components/WeatherApp";
+
+const locationState: Location = {
+  distance: 0,
+  title: "Tokyo",
+  location_type: "City",
+  woeid: 1118370,
+  latt_long: "35.670479,139.740921",
+};
+
+export const locationReducer = (
+  state = locationState,
+  { type, payload }: { type: string; payload: Location }
+) => {
+  switch (type) {
+    case LOCATION_ACTION_TYPE.SAVE_LOCATION_DATA:
+      return { ...state, ...payload };
+
+    default:
+      return state;
+  }
+};
