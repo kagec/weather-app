b274b901a33f6d3d0739fbfd7aec6998cb0c1451
kagec <66302618+kagec@users.noreply.github.com>
2021-09-23 16:21:10 +0900

    `reducer`の戻り型`Reducer`を削除

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -2,8 +2,6 @@ import { combineReducers } from "redux";
 import { weatherReducer } from "../reducer/weatherReducer";
 import { locationReducer } from "./locationReducer";
 
-export type Reducer = ReturnType<typeof reducer>;
-
 export const reducer = combineReducers({
   weather: weatherReducer,
   location: locationReducer,
