b314133df05dabff88c6d3d441cc348c422411d6
kagec <66302618+kagec@users.noreply.github.com>
2021-09-23 16:20:42 +0900

    `store`の戻り型`RootState`を追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { reducer } from "../reducer";
+
+export type RootState = ReturnType<typeof store.getState>;
+
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk));
