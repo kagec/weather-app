647b6a6ab588bc0304a0efe51dcd474adc8acc02
kagec <66302618+kagec@users.noreply.github.com>
2021-09-27 22:22:42 +0900

    `combineReducer`の引数に`isSearchReducer`を追加

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { weatherReducer } from "../reducer/weatherReducer";
 import { locationReducer } from "./locationReducer";
+import { isSearchReducer } from "./isSearchReducer";
 
 export const reducer = combineReducers({
   weather: weatherReducer,
   location: locationReducer,
+  isSearch: isSearchReducer,
 });
