9b0320e40f695783aa77c71dc08cac2448dd609b
kagec <66302618+kagec@users.noreply.github.com>
2021-09-27 19:04:05 +0900

    `onSubmit`関数を追加し、`form`タグに設定

--- src/components/SearchLocation.tsx
+++ src/components/SearchLocation.tsx
@@ -1,10 +1,36 @@
 import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { saveWeatherData } from "../action/weather";
+import { saveLocationData } from "../action/location";
+import type { FormEvent } from "react";
 
 const SearchLocation = () => {
   const [location, setLocation] = useState<string>("");
+  const dispatch = useDispatch();
+
+  const onSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void> = async (
+    e
+  ) => {
+    e.preventDefault();
+
+    try {
+      const locationData = await axios.get(
+        `/api/location/search/?query=${location}`
+      );
+      dispatch(saveLocationData(locationData.data[0]));
+
+      const weatherData = await axios.get(
+        `/api/location/${locationData?.data[0]?.woeid}`
+      );
+      dispatch(saveWeatherData(weatherData.data.consolidated_weather));
+    } catch (e) {
+      alert(e);
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="search location"
