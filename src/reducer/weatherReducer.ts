26c16d53e2cb06f198b3c663b698f0b4b4dc9b6a
kagec <66302618+kagec@users.noreply.github.com>
2021-09-29 18:18:45 +0900

    `weatherReducer`の初期ステートの型を（`{cityName :ConsolidatedWeather}`）になるように変更

--- src/reducer/weatherReducer.ts
+++ src/reducer/weatherReducer.ts
@@ -1,13 +1,17 @@
 import { WeatherAction, WEATHER_ACTION_TYPE } from "../action/weather";
 import type { ConsolidatedWeather } from "../components/WeatherApp";
 
+export interface WeatherState {
+  [key: string]: ConsolidatedWeather;
+}
+
 export const weatherReducer = (
-  state: ConsolidatedWeather = [],
+  state: WeatherState = {},
   { type, payload }: WeatherAction
 ) => {
   switch (type) {
     case WEATHER_ACTION_TYPE.SAVE_WEATHER_DATA:
-      return [...state, ...payload];
+      return { ...state, ...payload };
     default:
       return state;
   }
