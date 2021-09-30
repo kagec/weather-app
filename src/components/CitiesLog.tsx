0f0f7a3458b44a15ded4482ac9dd34a0ca9a6bad
kagec <66302618+kagec@users.noreply.github.com>
2021-09-30 18:49:30 +0900

    `CityButton`スタイルコンポーネントを追加

--- src/components/CitiesLog.tsx
+++ src/components/CitiesLog.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import chevronRight from "../image/baseline_chevron_right_white_24dp.png";
 
 const CitiesLog = () => {
   const weatherData = useSelector((state) => state.weather);
@@ -9,7 +10,7 @@ const CitiesLog = () => {
     <CitiesLogList>
       {cities.map((city) => (
         <li>
-          <button>{city}</button>
+          <CityButton>{city}</CityButton>
         </li>
       ))}
     </CitiesLogList>
@@ -20,6 +21,26 @@ const CitiesLogList = styled.ul`
   list-style: none;
   margin-top: 58px;
   padding: 0px;
+`;
+
+const CityButton = styled.button`
+  width: 100%;
+  color: #e7e7eb;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  height: 64px;
+  text-align: left;
+  border: transparent;
+  background-color: transparent;
+  word-break: break-word;
+
+  &:hover {
+    border: 1px solid #616475;
+    background-image: url(${chevronRight});
+    background-repeat: no-repeat;
+    background-position: right;
+  }
 `;
 
 export default CitiesLog;
