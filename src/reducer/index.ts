0ea867dddd5066e2966d605e463fc49cbdffebd7
kagec <66302618+kagec@users.noreply.github.com>
2021-10-03 23:02:48 +0900

    `combineReducers`に`citiesLogReducer`を追加

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { weatherReducer } from "../reducer/weatherReducer";
 import { locationReducer } from "./locationReducer";
 import { isSearchReducer } from "./isSearchReducer";
+import { citiesLogReducer } from "./citiesLogReducer";
 
 export const reducer = combineReducers({
   weather: weatherReducer,
   location: locationReducer,
   isSearch: isSearchReducer,
+  citiesLog: citiesLogReducer,
 });
