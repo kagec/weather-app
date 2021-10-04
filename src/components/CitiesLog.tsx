99ff1d054a94ba1e540a1d44b3f5f07cb09e42f8
kagec <66302618+kagec@users.noreply.github.com>
2021-10-04 17:30:03 +0900

    `fetchWeatherDataByCityName`を追加し、`onClick`に設定

--- src/components/CitiesLog.tsx
+++ src/components/CitiesLog.tsx
@@ -1,15 +1,46 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import type { Dispatch } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { addCityLog } from "../action/citiesLog";
+import { saveLocationData } from "../action/location";
+import { saveWeatherData } from "../action/weather";
 import chevronRight from "../image/baseline_chevron_right_white_24dp.png";
+
+export const fetchWeatherDataByCityName: (
+  cityName: string,
+  dispatch: Dispatch<any>
+) => Promise<void> = async (cityName, dispatch) => {
+  try {
+    const locationData = await axios.get(
+      `/api/location/search/?query=${cityName}`
+    );
+    dispatch(saveLocationData(locationData.data[0]));
+    dispatch(addCityLog(locationData.data[0].title));
+
+    const weatherData = await axios.get(
+      `/api/location/${locationData?.data[0]?.woeid}`
+    );
+
+    dispatch(saveWeatherData(weatherData.data.consolidated_weather));
+  } catch (e) {
+    alert(e);
+  }
+};
 
 const CitiesLog = () => {
   const cities = useSelector((state) => state.citiesLog);
+  const dispatch = useDispatch();
 
   return (
     <CitiesLogList>
       {cities.map((city, index) => (
         <li key={index}>
-          <CityButton>{city}</CityButton>
+          <CityButton
+            onClick={() => fetchWeatherDataByCityName(city, dispatch)}
+          >
+            {city}
+          </CityButton>
         </li>
       ))}
     </CitiesLogList>
