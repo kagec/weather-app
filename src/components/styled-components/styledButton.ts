42dd1a2dbf2e09c019c25fb3cb62d917220cef6c
kagec <66302618+kagec@users.noreply.github.com>
2021-10-04 18:01:24 +0900

    基礎となる`Button`スタイルコンポーネントを追加

---
+++ src/components/styled-components/styledButton.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-size: 16px;
+  line-height: var(--base-line-height);
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
