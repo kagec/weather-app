ad974ae99e0664afea0e0dcc2a588e14721992ed
kagec <66302618+kagec@users.noreply.github.com>
2021-10-05 12:44:59 +0900

    `Button`スタイルコンポーネントに`font-family`を追加

--- src/components/styled-components/styledButton.ts
+++ src/components/styled-components/styledButton.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Button = styled.button`
   font-size: 16px;
   line-height: var(--base-line-height);
+  font-family: var(--base-font);
   color: #e7e7eb;
 
   &:hover {
