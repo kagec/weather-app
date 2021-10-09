6c99be3f4c28000681c23846aefb475dd2f9b5fe
kagec <66302618+kagec@users.noreply.github.com>
2021-10-09 20:45:44 +0900

    `entities`アクションファイルを作成し、`saveLocationData`と`saveWeatherData`アクションクリエイターを追加

---
+++ src/action/entities.ts
@@ -0,0 +1,20 @@
+import type { ConsolidatedWeather, Location } from "../components/WeatherApp";
+
+export type EntitiesAction =
+  | ReturnType<typeof saveLocationData>
+  | ReturnType<typeof saveWeatherData>;
+
+export const ENTITIES_ACTION_TYPE = {
+  SAVE_LOCATION_DATA: "SAVE_LOCATION_DATA",
+  SAVE_WEATHER_DATA: "SAVE_WEATHER_DATA:",
+} as const;
+
+export const saveLocationData = (locationData: Location) => ({
+  type: ENTITIES_ACTION_TYPE.SAVE_LOCATION_DATA,
+  payload: locationData,
+});
+
+export const saveWeatherData = (weatherData: ConsolidatedWeather) => ({
+  type: ENTITIES_ACTION_TYPE.SAVE_WEATHER_DATA,
+  payload: weatherData,
+});
