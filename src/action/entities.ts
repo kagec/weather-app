05a6bc892a0d9de21967e14580b5ee1f1de041c5
kagec <66302618+kagec@users.noreply.github.com>
2021-10-09 22:26:12 +0900

    `selectCurrentWoeid`アクションクリエイターを追加

--- src/action/entities.ts
+++ src/action/entities.ts
@@ -2,11 +2,13 @@ import type { ConsolidatedWeather, Location } from "../components/WeatherApp";
 
 export type EntitiesAction =
   | ReturnType<typeof saveLocationData>
-  | ReturnType<typeof saveWeatherData>;
+  | ReturnType<typeof saveWeatherData>
+  | ReturnType<typeof selectCurrentWoeid>;
 
 export const ENTITIES_ACTION_TYPE = {
   SAVE_LOCATION_DATA: "SAVE_LOCATION_DATA",
   SAVE_WEATHER_DATA: "SAVE_WEATHER_DATA:",
+  SELECT_CURRENT_WOEID: "SELECT_CURRENT_WOEID",
 } as const;
 
 export const saveLocationData = (locationData: Location) => ({
@@ -20,4 +22,9 @@ export const saveWeatherData = (
 ) => ({
   type: ENTITIES_ACTION_TYPE.SAVE_WEATHER_DATA,
   payload: { weatherData, woeid },
+});
+
+export const selectCurrentWoeid = (woeid: number) => ({
+  type: ENTITIES_ACTION_TYPE.SELECT_CURRENT_WOEID,
+  payload: woeid,
 });
