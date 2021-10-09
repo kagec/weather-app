54ad0b2fff9c753857428fc38544b1eb010e6fd4
kagec <66302618+kagec@users.noreply.github.com>
2021-10-09 20:42:58 +0900

    `combineReducers`に`entitiesReducer`を追加

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -3,10 +3,12 @@ import { weatherReducer } from "../reducer/weatherReducer";
 import { locationReducer } from "./locationReducer";
 import { isSearchReducer } from "./isSearchReducer";
 import { citiesLogReducer } from "./citiesLogReducer";
+import { entitiesReducer } from "./entitiesReducer";
 
 export const reducer = combineReducers({
   weather: weatherReducer,
   location: locationReducer,
   isSearch: isSearchReducer,
   citiesLog: citiesLogReducer,
+  entities: entitiesReducer,
 });
