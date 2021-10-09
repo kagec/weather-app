966af3d00dea15f397d60bdaf85a7d47d87b8203
kagec <66302618+kagec@users.noreply.github.com>
2021-10-10 00:53:35 +0900

    アクションクリエイターを`action/location`と`action/weather`のものから`action/entities`へ変更

--- src/components/SearchLocation.tsx
+++ src/components/SearchLocation.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { saveWeatherData } from "../action/weather";
-import { saveLocationData } from "../action/location";
 import type { FormEvent } from "react";
 import { isSearchOff } from "../action/isSearch";
 import styled from "styled-components";
 import CitiesLog from "./CitiesLog";
 import { addCityLog } from "../action/citiesLog";
 import { Button } from "./styled-components/styledButton";
+import { saveLocationData, saveWeatherData } from "../action/entities";
 
 const SearchLocation = () => {
   const [location, setLocation] = useState<string>("");
@@ -36,7 +35,12 @@ const SearchLocation = () => {
       const weatherData = await axios.get(
         `/api/location/${locationData?.data[0]?.woeid}`
       );
-      dispatch(saveWeatherData(weatherData.data.consolidated_weather));
+      dispatch(
+        saveWeatherData(
+          weatherData.data.consolidated_weather,
+          locationData.data[0].woeid
+        )
+      );
       dispatch(isSearchOff());
     } catch (e) {
       alert(e);
