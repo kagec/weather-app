52c2615b3d533cf89875a771f2fae88585859720
kagec <66302618+kagec@users.noreply.github.com>
2021-10-11 22:00:00 +0900

    不要になったリデューサーとインポートを削除

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,13 +1,10 @@
 import { combineReducers } from "redux";
-import { weatherReducer } from "../reducer/weatherReducer";
-import { locationReducer } from "./locationReducer";
+
 import { isSearchReducer } from "./isSearchReducer";
 import { citiesLogReducer } from "./citiesLogReducer";
 import { entitiesReducer } from "./entitiesReducer";
 
 export const reducer = combineReducers({
-  weather: weatherReducer,
-  location: locationReducer,
   isSearch: isSearchReducer,
   citiesLog: citiesLogReducer,
   entities: entitiesReducer,
