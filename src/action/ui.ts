03926966dadb5862cc8ddb8b422ff057b9227a84
kagec <66302618+kagec@users.noreply.github.com>
2021-10-14 10:54:10 +0900

    `ui`アクションファイルを作成し、`isSearch`のアクションクリエイターを追加

---
+++ src/action/ui.ts
@@ -0,0 +1,18 @@
+export type UIAction =
+  | ReturnType<typeof isSearchOn>
+  | ReturnType<typeof isSearchOff>;
+
+export const UI_ACTION_TYPE = {
+  IS_SEARCH_ON: "IS_SEARCH_ON",
+  IS_SEARCH_OFF: "IS_SEARCH_OFF",
+} as const;
+
+export const isSearchOn = () => ({
+  type: UI_ACTION_TYPE.IS_SEARCH_ON,
+  payload: true,
+});
+
+export const isSearchOff = () => ({
+  type: UI_ACTION_TYPE.IS_SEARCH_OFF,
+  payload: false,
+});
