9e0e303da5c4cc3532c33c8716543e6673f46e86
kagec <66302618+kagec@users.noreply.github.com>
2021-10-17 17:59:52 +0900

    天気データを表示する機能を追加

--- src/components/NextWeather.tsx
+++ src/components/NextWeather.tsx
@@ -1,5 +1,33 @@
+import { useSelector } from "react-redux";
+import type { ConsolidatedWeather } from "./WeatherApp";
+import { changeDateFormat, getWeatherImage } from "./TodayWeather";
+
 const NextWeather = () => {
-  return;
+  const [weathers, selectedWoeid]: [
+    { [key: number]: ConsolidatedWeather },
+    number
+  ] = useSelector((state) => [
+    state.entities.weathers.byWoeid,
+    state.entities.locations.selectedWoeid,
+  ]);
+  const weather = weathers?.[selectedWoeid]?.slice(1);
+
+  return !weather ? (
+    <div>Loading...</div>
+  ) : (
+    <ul>
+      {weather.map((weather) => (
+        <li key={weather.applicable_date}>
+          <div>{changeDateFormat(weather.applicable_date)}</div>
+          <div>{getWeatherImage(weather.weather_state_abbr)}</div>
+          <div>
+            {Math.round(weather.max_temp)}℃
+            <span>{Math.round(weather.min_temp)}℃</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default NextWeather;
