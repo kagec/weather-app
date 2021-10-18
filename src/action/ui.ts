04c33d75e38179bff23972c2b0d81b9c8621b578
kagec <66302618+kagec@users.noreply.github.com>
2021-10-18 16:11:29 +0900

    `changeDegreeToCelsius`アクションクリエイターを追加

--- src/action/ui.ts
+++ src/action/ui.ts
@@ -1,10 +1,12 @@
 export type UIAction =
   | ReturnType<typeof toggleSearch>
-  | ReturnType<typeof changeDegreeToFahrenheit>;
+  | ReturnType<typeof changeDegreeToFahrenheit>
+  | ReturnType<typeof changeDegreeToCelsius>;
 
 export const UI_ACTION_TYPE = {
   TOGGLE_SHOW_SEARCH: "TOGGLE_SHOW_SEARCH",
   CHANGE_DEGREE_FAHRENHEIT: "CHANGE_DEGREE_FAHRENHEIT",
+  CHANGE_DEGREE_CELSIUS: "CHANGE_DEGREE_CELSIUS",
 } as const;
 
 export const toggleSearch = () => ({
@@ -13,4 +15,8 @@ export const toggleSearch = () => ({
 
 export const changeDegreeToFahrenheit = () => ({
   type: UI_ACTION_TYPE.CHANGE_DEGREE_FAHRENHEIT,
+});
+
+export const changeDegreeToCelsius = () => ({
+  type: UI_ACTION_TYPE.CHANGE_DEGREE_CELSIUS,
 });
