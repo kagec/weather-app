a6aa4314ebfd991311b9fb923eda2c956ed62031
kagec <66302618+kagec@users.noreply.github.com>
2021-10-18 11:43:53 +0900

    `CelsiusButton`スタイルコンポーネントを作成し、適用

--- src/components/ChangeDegree.tsx
+++ src/components/ChangeDegree.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { Button } from "./styled-components/styledButton";
 
 const ChangeDegree = () => {
   return (
     <ChangeDegreeContainer>
-      <button>℃</button>
+      <CelsiusButton>℃</CelsiusButton>
       <button>℉</button>
     </ChangeDegreeContainer>
   );
@@ -12,6 +13,17 @@ const ChangeDegree = () => {
 const ChangeDegreeContainer = styled.div`
   display: flex;
   justify-content: flex-end;
+`;
+
+const CelsiusButton = styled(Button)`
+  width: 40px;
+  height: 40px;
+  font-weight: 700;
+  font-size: 18px;
+  color: #110e3c;
+  background: #e7e7eb;
+  border-radius: 50px;
+  border-color: transparent;
 `;
 
 export default ChangeDegree;
