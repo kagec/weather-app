89a984f56809538d05d245a60443113a5d9a61a6
kagec <66302618+kagec@users.noreply.github.com>
2021-10-18 14:42:59 +0900

    `changeDegreeToFahrenheit`アクションクリエイターを追加

--- src/action/ui.ts
+++ src/action/ui.ts
@@ -1,9 +1,16 @@
-export type UIAction = ReturnType<typeof toggleSearch>;
+export type UIAction =
+  | ReturnType<typeof toggleSearch>
+  | ReturnType<typeof changeDegreeToFahrenheit>;
 
 export const UI_ACTION_TYPE = {
   TOGGLE_SHOW_SEARCH: "TOGGLE_SHOW_SEARCH",
+  CHANGE_DEGREE_FAHRENHEIT: "CHANGE_DEGREE_FAHRENHEIT",
 } as const;
 
 export const toggleSearch = () => ({
   type: UI_ACTION_TYPE.TOGGLE_SHOW_SEARCH,
+});
+
+export const changeDegreeToFahrenheit = () => ({
+  type: UI_ACTION_TYPE.CHANGE_DEGREE_FAHRENHEIT,
 });
