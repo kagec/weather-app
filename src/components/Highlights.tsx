ed13509c35d5cf5a9f9ffdc9ca2749fd7031a7cc
kagec <66302618+kagec@users.noreply.github.com>
2021-10-22 16:00:21 +0900

    `Tile`スタイルコンポーネントとその派生コンポーネントを作成
    かぶっているプロパティを`Tile`コンポーネントにまとめ、差分を派生コンポーネントで実装

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -18,7 +18,7 @@ const Highlights = () => {
     <HighlightWrapper>
       <HighlightHeader>Today's Highlights</HighlightHeader>
       <HightlightContainer>
-        <div>
+        <LargeTile>
           <div>Wind status</div>
           <div>
             {Math.round(weather.wind_speed)}
@@ -28,28 +28,28 @@ const Highlights = () => {
             <span className="material-icons">navigation</span>
             {weather.wind_direction_compass}
           </div>
-        </div>
-        <div>
+        </LargeTile>
+        <LargeTile>
           <div>Humidity</div>
           <div>
             {weather.humidity}
             <span>%</span>
           </div>
-        </div>
-        <div>
+        </LargeTile>
+        <SmallTile>
           <div>Visibility</div>
           <div>
             {(Math.round(weather.visibility * 10) / 10).toLocaleString("de-DE")}
             <span> miles</span>
           </div>
-        </div>
-        <div>
+        </SmallTile>
+        <SmallTile>
           <div>Air Pressure</div>
           <div>
             {Math.round(weather.air_pressure).toLocaleString("de-DE")}
             <span> mb</span>
           </div>
-        </div>
+        </SmallTile>
       </HightlightContainer>
     </HighlightWrapper>
   );
@@ -70,6 +70,20 @@ const HightlightContainer = styled.div`
   flex-wrap: wrap;
   margin-top: 32px;
   gap: 48px;
+`;
+
+const Tile = styled.div`
+  background: #1e213a;
+  width: 328px;
+  text-align: center;
+`;
+
+const LargeTile = styled(Tile)`
+  height: 204px;
+`;
+
+const SmallTile = styled(Tile)`
+  height: 159px;
 `;
 
 export default Highlights;
