7fa02c0b66903e118aa377d0d249e9334dcafbd2
kagec <66302618+kagec@users.noreply.github.com>
2021-10-25 13:37:04 +0900

    `FooterSign`を作成し、フッターの位置とフォント関連のスタイルを指定

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,24 @@
+import styled from "styled-components";
+
 const Footer = () => {
   return (
-    <footer>
+    <FooterSign>
       created by <span>kagec</span> - devChallenges.io
-    </footer>
+    </FooterSign>
   );
 };
+
+const FooterSign = styled.footer`
+  color: #a09fb1;
+  font-family: Montserrat;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  margin-top: 112px;
+
+  > span {
+    font-weight: 700;
+  }
+`;
 
 export default Footer;
